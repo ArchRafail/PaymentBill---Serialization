c1dc3be7e8052459f4cfaba9ad579de38ad6bddf
Roman <Bilo_um18@student.itstep.org>
2021-11-12 22:42:19 +0200

    Add project files.

---
+++ PaymentBill - Serialization/PaymentBill.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PaymentBill___Serialization
+{
+    [Serializable]
+    class PaymentBill : ISerializable
+    {
+        double each_day_payment;
+        int days_quantity;
+        double fine_tax;
+        int days_payment_delay;
+        double direct_payment;
+        double fine;
+        double total_payment;
+        static bool flag;
+        static bool Flag
+        {
+            get => flag;
+            set => flag = value;
+        }
+
+        public PaymentBill() { }
+
+        public PaymentBill(double edp, int dq, double ft, int dpd, bool flag = true)
+        {
+            each_day_payment = edp;
+            days_quantity = dq;
+            fine_tax = ft;
+            days_payment_delay = dpd;
+            direct_payment = 0;
+            fine = 0;
+            total_payment = 0;
+            Flag = flag;
+        }
+
+        private PaymentBill(SerializationInfo info, StreamingContext context)
+        {
+            each_day_payment = info.GetDouble("Each day payment");
+            days_quantity = info.GetInt32("Days quantity");
+            fine_tax = info.GetDouble("Fine tax");
+            days_payment_delay = info.GetInt32("Days payment delay");
+            if (Flag)
+            {
+                direct_payment = info.GetDouble("Direct payment");
+                fine = info.GetDouble("Fine");
+                total_payment = info.GetDouble("Total payment");
+            }
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Each day payment", each_day_payment);
+            info.AddValue("Days quantity", days_quantity);
+            info.AddValue("Fine tax", fine_tax);
+            info.AddValue("Days payment delay", days_payment_delay);
+            if (Flag)
+            {
+                direct_payment = each_day_payment * days_quantity;
+                fine = fine_tax * days_payment_delay;
+                total_payment = direct_payment + fine;
+                info.AddValue("Direct payment", direct_payment);
+                info.AddValue("Fine", fine);
+                info.AddValue("Total payment", total_payment);
+            }
+        }
+
+        public bool GetFlag() => Flag;
+
+        public override string ToString()
+        {
+            return $"Each day payment: {each_day_payment}\nDays quantity: {days_quantity}\n" +
+                $"Fine tax: {fine_tax}\nDays payment delay: {days_payment_delay}\n" +
+                $"Direct payment: {direct_payment}\nFine: {fine}\nTotal payment: {total_payment}\n";
+        }
+    }
+}
---
+++ PaymentBill - Serialization/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PaymentBill___Serialization
+{
+    class Program
+    {
+        static int GetIntInfo(string text)
+        {
+            int value;
+            Console.Write($"Input {text}. Value must be > 0. : ");
+            do
+            {
+                value = Convert.ToInt32(Console.ReadLine());
+            } while (value <= 0);
+            return value;
+        }
+        static double GetDoubleInfo(string text)
+        {
+            double value;
+            Console.Write($"Input {text}. Value must be > 0. : ");
+            do
+            {
+                value = Convert.ToDouble(Console.ReadLine());
+            } while (value <= 0);
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program is about serialization an information.");
+            Console.WriteLine("The process of q-ty info to serialization depends on flag.");
+            Console.WriteLine("Default set of the flag is true.");
+            Console.WriteLine("If you want to change flag - input or delete false in constructor of payment object.\n");
+            Console.WriteLine("An information about serialization saves in file PaymentBill.bin in default project folder.\n");
+
+            PaymentBill payment = new PaymentBill(GetDoubleInfo("each day payment"), GetIntInfo("days quantity"),
+                GetDoubleInfo("fine tax"), GetIntInfo("days payment delay"), false);  //false - flag of serialization q-ty info
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            string file_path = "PaymentBill.bin";
+
+            try
+            {
+                using (FileStream fstream = new FileStream(file_path,
+                    FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    formatter.Serialize(fstream, payment);
+                }
+
+                Console.WriteLine($"\nThe info is binary serialized with bool flag {payment.GetFlag()}.\n");
+
+                PaymentBill payment1 = null;
+
+                using (FileStream fstream = new FileStream(file_path,
+                    FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    payment1 = (PaymentBill)formatter.Deserialize(fstream);
+                }
+
+                Console.WriteLine(payment1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
